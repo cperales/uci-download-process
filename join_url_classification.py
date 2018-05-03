438b39588f98728f95f2129f43503a5125120511
cperales <cperales@uloyola.es>
2018-05-03 12:02:56 +0200

    Python file to create a .txt with the urls to data

---
+++ join_url_classification.py
@@ -0,0 +1,25 @@
+import configparser
+import os
+
+folder_classification = 'datafiles/classification/'
+dataset_folders = list()
+list_files = list()
+
+for folder in os.listdir(folder_classification):
+    complete_folder = ''.join([folder_classification, folder])
+    if os.path.isdir(complete_folder):  # It could be a .csv
+        dataset_folders.append(folder)
+        files = os.listdir(complete_folder)
+        for file in files:
+            if '.ini' in file:  # It's the config file we're looking for
+                list_files.append('/'.join([complete_folder, file]))
+
+with open('wget_database.txt', 'w') as f:
+    for file in list_files:
+        config = configparser.ConfigParser()
+        config.read(file)
+        try:
+            data_url = config['info']['data_url']
+            f.write(''.join([data_url, '\n']))
+        except Exception as e:
+            print(e)
