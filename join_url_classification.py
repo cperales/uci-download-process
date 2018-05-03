c80ed07027aed7dfb6e973194a29fe5b1f9e0a36
cperales <cperales@uloyola.es>
2018-05-03 13:16:42 +0200

    Read and download .data files

--- join_url_classification.py
+++ join_url_classification.py
@@ -1,4 +1,5 @@
 import configparser
+import subprocess
 import os
 
 folder_classification = 'datafiles/classification/'
@@ -14,12 +15,15 @@ for folder in os.listdir(folder_classification):
             if '.ini' in file:  # It's the config file we're looking for
                 list_files.append('/'.join([complete_folder, file]))
 
-with open('database/wget_database.txt', 'w') as f:
+with open('wget_database.txt', 'w') as f:
     for file in list_files:
         config = configparser.ConfigParser()
         config.read(file)
         try:
             data_url = config['info']['data_url']
-            f.write(''.join([data_url, '\n']))
+            if '.data' in data_url:
+                f.write(''.join([data_url, '\n']))
         except Exception as e:
             print(e)
+
+subprocess.run('wget -i wget_database.txt -P database/'.split())
