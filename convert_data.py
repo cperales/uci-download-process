397e263db7aacea8899b771b94aa6cb6fe9de79a
cperales <cperales@uloyola.es>
2018-05-03 17:16:11 +0200

    Parsing target position

--- convert_data.py
+++ convert_data.py
@@ -1,36 +1,53 @@
 import os
+import configparser
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-origin_folder = 'database/'
-final_folder = 'processed/'
+db_folder = 'database/'
+config_folder = 'datafiles/classification/'
+processed_folder = 'processed/'
 
-for file in os.listdir(origin_folder):
+for file in os.listdir(db_folder):
     try:
-        df = pd.read_csv(''.join([origin_folder, file]), sep=',', header=None)
-        if df[0].dtype != int and df[0].dtype != float:
-            le = LabelEncoder()
-            df[0] = le.fit_transform(df[0]) + 1
+        df = pd.read_csv(''.join([db_folder, file]), sep=',', header=None)
+        config_filename = file.replace('.data', '').lower()
+        config_file = '/'.join([''.join([config_folder, config_filename]),
+                                'config.ini'])
+        if os.path.isfile(config_file):
+            config = configparser.ConfigParser()
+            config.read(config_file)
+            # Changing label to last column
+            final_column = df.columns[-1]
+            label_column = int(config['info']['target_index'])
+            if label_column not in df.columns:
+                TypeError('Label index {} is not in columns {}'.format(label_column, df.columns))
+            a = df[final_column]
+            df[final_column] = df[label_column]
+            df[label_column] = a
 
-        for c in df.columns[1:]:
-            if df[c].dtype == float or df[c].dtype == int:  # It is a float, but it was parsed as int
-                df[c] = pd.Series(df[c], dtype=float)
-            else:  # It was a string, need to be transformed
+            if df[final_column].dtype != int and df[final_column].dtype != float:
                 le = LabelEncoder()
-                try:
-                    df[c] = pd.Series(le.fit_transform(df[c]) + 1, dtype=float)
-                except Exception as e:
-                    print(e)
+                df[final_column] = le.fit_transform(df[final_column]) + 1
 
-        # Changing label (first column) to last column
-        a = df[df.columns[-1]]
-        df[df.columns[-1]] = df[0]
-        df[0] = a
+            for c in df.columns[:final_column]:
+                if df[c].dtype == float or df[c].dtype == int:  # It is a float, but it was parsed as int
+                    df[c] = pd.Series(df[c], dtype=float)
+                else:  # It was a string, need to be transformed
+                    le = LabelEncoder()
+                    try:
+                        df[c] = pd.Series(le.fit_transform(df[c]) + 1, dtype=float)
+                    except Exception as e:
+                        print(e)
 
-        # Saving the dataframe
-        df.to_csv(''.join([final_folder, file]), sep=' ', header=False, index=False)
+            # Saving the dataframe
+            df.to_csv(''.join([processed_folder, file]), sep=' ', header=False, index=False)
+        else:
+            print(config_file, ' not found')
     except pd.errors.ParserError:
         print(' '.join([file, 'gives a parser error']))
 
     except UnicodeDecodeError:
         print(' '.join([file, 'gives an Unicode error']))
+
+    except TypeError:
+        pass
