21007ce4ee2233b9c160c1499e97b6cff71e9dff
cperales <cperales@uloyola.es>
2018-06-05 19:14:03 +0200

    Table with info is generated into a .csv

--- data_characteristics.py
+++ data_characteristics.py
@@ -1,16 +1,40 @@
-import os
 import subprocess
 import glob
+import pandas as pd
+import numpy as np
 
 full_data_files = glob.glob('data/*/*.data')
 data_files = [d.split('/')[2].split('.')[0] for d in full_data_files]
 
-data_dict = dict()
+data_list = list()
 for d in full_data_files:
     name = d.split('/')[2].split('.')[0]
-    command = ['wc', d, '-l']
-    f = subprocess.Popen(command, stdout=subprocess.PIPE).stdout
-    output = str(f.read().splitlines()[0])
-    print(output)
-    lines, pathfile = output.split(' ')
-    data_dict.update({name: {'lines': lines}})
+    # command = ['wc', d, '-l']
+    # f = subprocess.Popen(command, stdout=subprocess.PIPE).stdout
+    # output = str(f.read().splitlines()[0])
+    # lines, pathfile = output.split(' ')
+    # # PANDAS
+    df = pd.read_csv(d,
+                     sep='\s+',
+                     header=None)
+    lines, columns = df.shape
+    attribute = columns - 1  # Minus one because of target
+    last_pos = attribute
+    classes = np.unique(df[last_pos])
+    n_classes = len(classes)
+    distribution = [len(df[df[last_pos] == c]) for c in classes]
+    data_list.append({'Dataset': name,
+                      'Size': lines,
+                      'Attributes': attribute,
+                      'Classes': n_classes,
+                      'Class distribution': distribution})
+
+pd.DataFrame(data_list).to_csv('data_characteristics.csv',
+                               sep=';',
+                               header=True,
+                               columns=['Dataset',
+                                        'Size',
+                                        'Attributes',
+                                        'Classes',
+                                        'Class distribution'],
+                               index=False)
