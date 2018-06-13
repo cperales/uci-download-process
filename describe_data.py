29ce2b9797f6050e73357a875e93e010ccc095bb
cperales <cperales@uloyola.es>
2018-06-13 18:02:21 +0200

    .tex file is not commited anymore.
    LaTeX generation is done in describe_data.py

--- describe_data.py
+++ describe_data.py
@@ -2,6 +2,9 @@ import os
 import glob
 import pandas as pd
 import numpy as np
+from pylatex import Document, LongTable
+from copy import deepcopy
+
 
 description_folder = 'description'
 
@@ -14,12 +17,6 @@ data_files = [d.split('/')[2].split('.')[0] for d in full_data_files]
 data_list = list()
 for d in full_data_files:
     name = d.split('/')[2].split('.')[0]
-    # # BASH
-    # command = ['wc', d, '-l']
-    # f = subprocess.Popen(command, stdout=subprocess.PIPE).stdout
-    # output = str(f.read().splitlines()[0])
-    # lines, pathfile = output.split(' ')
-    # # PANDAS
     df = pd.read_csv(d,
                      sep='\s+',
                      header=None)
@@ -35,13 +32,83 @@ for d in full_data_files:
                       'Classes': n_classes,
                       'Class distribution': distribution})
 
-pd.DataFrame(data_list).to_csv(os.path.join(description_folder,
-                                            'data_description.csv'),
-                               sep=';',
-                               header=True,
-                               columns=['Dataset',
-                                        'Size',
-                                        'Attributes',
-                                        'Classes',
-                                        'Class distribution'],
-                               index=False)
+df = pd.DataFrame(data_list)
+df = df.sort_values('Size', ascending=False)
+cols = ['Dataset',
+        'Size',
+        'Attributes',
+        'Classes',
+        'Class distribution']
+df = df[cols]
+df_copy = deepcopy(df)
+df_copy['Class distribution'] = [str(value).replace(', ', ';').replace(')', '').replace('(', '')
+                                 for value in df_copy['Class distribution']]
+df_copy.to_csv(os.path.join(description_folder,
+                            'data_description.csv'),
+               sep=',',
+               header=True,
+               columns=['Dataset',
+                        'Size',
+                        'Attributes',
+                        'Classes',
+                        'Class distribution'],
+               index=False)
+
+# # LaTeX
+df = df.set_index(['Dataset'])
+
+# Max classes per row
+max_classes = 3
+geometry_options = {
+    "margin": "1.00cm",
+    "includeheadfoot": True
+}
+doc = Document(page_numbers=True, geometry_options=geometry_options)
+
+# Generate data table
+with doc.create(LongTable("l l l l l")) as data_table:
+        data_table.add_hline()
+        header = ["Dataset",
+                  "Size",
+                  "#Attr.",
+                  "#Classes",
+                  "Class distribution"]
+        data_table.add_row(header)
+        data_table.add_hline()
+        data_table.add_hline()
+        for index in df.index.values:
+            row = [index] + df.loc[index].values.tolist()
+            if len(row[-1]) > max_classes:
+                max = max_classes
+                finished = False
+                subrow = row.copy()
+                # Select subtuple and removing last parenthesis
+                subrow[-1] = str(subrow[-1][:max]).replace(')', ',')
+                data_table.add_row(subrow)
+                while finished is False:
+                    last_element = row[-1][max:max + max_classes]
+                    if len(last_element) == 1:
+                        # To string
+                        last_element = str(last_element)
+                        # Remove first and last parenthesis and comma
+                        last_element = last_element[1:-2]
+                    else:
+                        # To string
+                        last_element = str(last_element)
+                        # Remove first and last parenthesis
+                        last_element = last_element[1:-1]
+                    max = max + max_classes
+                    if max >= len(row[-1]):
+                        finished = True
+                        last_element += ')'
+                    else:
+                        # Remove last parenthesis or comma if len is 1
+                        last_element = last_element[:-1]
+                    subrow = ['', '', '', '', last_element]
+                    data_table.add_row(subrow)
+
+            else:
+                data_table.add_row(row)
+
+doc.generate_pdf(os.path.join(description_folder,
+                              'data_description'), clean_tex=False)
