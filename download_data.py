b3eb1ff912df35effb8c93dc1ef51bacae2e069e
cperales <sir.perales@gmail.com>
2019-07-11 11:52:59 +0200

    Now it untar

--- download_data.py
+++ download_data.py
@@ -12,29 +12,62 @@ def download_files(config_folder,
 
     with open(log_file, 'w') as f:
         for folder in os.listdir(config_folder):
-            complete_folder = ''.join([config_folder, folder])
+            complete_folder = os.path.join(config_folder, folder)
             if os.path.isdir(complete_folder):  # It could be a .csv
                 dataset_folders.append(folder)
-                files = os.listdir(complete_folder)
-                for file in files:
-                    if '.ini' in file:  # It's the config file we're looking for
-                        config_file = '/'.join([complete_folder, file])
-                        list_files.append(config_file)
-                        config = configparser.ConfigParser()
-                        config.read(config_file)
-                        try:
-                            data_url = config['info']['data_url']
-                            data_name = config['info']['name']
-                            f.write(''.join([data_url, '\n']))
-                            final_filename = os.path.join(complete_folder, data_name)
-                            # Download file
-                            bash_command = ['wget', '-nc', data_url, '-O', final_filename]
-                            subprocess.run(bash_command)
-                            # Copy file to raw data folder
-                            final_filename_2 = os.path.join(raw_data_folder, data_name)
-                            shutil.copyfile(final_filename, final_filename_2)
-                        except Exception as e:
-                            print(e)
+                config_file = os.path.join(complete_folder, 'config.ini')
+                list_files.append(config_file)
+                config = configparser.ConfigParser()
+                config.read(config_file)
+                try:
+                    data_url = config['info']['data_url']
+                    data_download = os.path.split(config['info']['data_url'])[-1]
+                    data_name = config['info']['name']
+                    f.write(''.join([data_url, '\n']))
+                    final_filename = os.path.join(complete_folder, data_download)
+                    # Download file
+                    bash_command = ['wget', '-nc', data_url, '-O', final_filename]
+                    subprocess.run(bash_command)
+                    # Extract data if necessary
+                    if '.tar.gz' in data_download:
+                        extract_tar(complete_folder=complete_folder,
+                                    tar_file=data_download,
+                                    data_name=data_name)
+                    elif data_name != data_download:
+                        os.rename(final_filename,
+                                  os.path.join(complete_folder, data_name))
+                    # Copy file to raw data folder
+                    final_filename_2 = os.path.join(raw_data_folder, data_name)
+                    shutil.copyfile(final_filename, final_filename_2)
+                except Exception as e:
+                    print(e)
+
+
+def extract_tar(complete_folder, tar_file, data_name):
+    """
+    Extract tar.gz file and get the data.
+    :param tar_file:
+    :return:
+    """
+    files_folder = os.listdir(complete_folder)
+    tar_file_path = os.path.join(complete_folder, tar_file)
+    bash_command = ['tar', 'xzf', tar_file_path,'-C', complete_folder]
+    subprocess.run(bash_command)
+    new_files_folder = os.listdir(complete_folder)
+    for f in new_files_folder:
+        if f not in files_folder:  # Extracted file or folder
+            if data_name == f:  # It is the file we were expecting
+                break
+            elif os.path.isdir(f):  # It is a folder
+                subfolder_path = os.path.join(complete_folder, f)
+                files_in_folder = os.listdir(subfolder_path)
+                for f_s in files_in_folder:
+                    if data_name == f_s:
+                        shutil.move(os.path.join(subfolder_path, data_name),
+                                    os.path.join(complete_folder, data_name))
+                        shutil.rmtree(subfolder_path)
+                        os.remove(tar_file_path)
+                        break
 
 
 def remove_files(config_folder):
